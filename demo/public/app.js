f7ced87813fbe3320078a450254f2d5faf438624
James Churchill <james@smashdev.com>
2017-02-28 15:31:38 -0800

    Updated demo

--- demo/public/app.js
+++ demo/public/app.js
@@ -1,14 +1,15 @@
 
 getData().done(function (data) {
-  const booksDiv = $('#books');
+  let booksContent = '';
   data.data.forEach(function (book) {
-    booksDiv.append(`<h2>${book.title}</h2>`);
+    booksContent += `<h2>${book.title}</h2>`;
     if (book.ratings.length > 0) {
       let ratingsContent = '';
       book.ratings.forEach(function (rating) {
         ratingsContent += `<li>Rating: ${rating.rating} - <i>${rating.comment}</i> - <b>${rating.username}</b></li>`;
       });
-      booksDiv.append(`<ul>${ratingsContent}</ul>`);
+      booksContent += `<ul>${ratingsContent}</ul>`;
     }
   });
+  $('#books').append(booksContent);
 });
